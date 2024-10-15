07a226758b24adc86fa047d6daf172f7eb479e64
jmau949 <44676775+jmau949@users.noreply.github.com>
2024-10-14 19:43:44 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
 export { Button } from "./components/button/button";
+export { Icon, IconTypes } from "./components/icon/icon";
+export { Input } from "./components/input/input";
+export { Label } from "./components/label/label";
+export { Pagination } from "./components/pagination/pagination";
+export { Select } from "./components/select/select";
+export { Table } from "./components/table/table";
+export { Cell } from "./components/table/cell";
+export { HeaderCell } from "./components/table/header-cell";
+export { Row } from "./components/table/row";
+export { TableProgress } from "./components/table/table-progress";
+export { DataGrid } from "./components/data-grid/data-grid";
+export { DataGridCell } from "./components/data-grid/data-grid-cell";
+export { DataGridRow } from "./components/data-grid/data-grid-row";
+export { Dialog, useDialog } from "./components/dialog/dialog";
+export { DialogForm } from "./components/dialog/dialog-form";
+export { DialogFooter } from "./components/dialog/dialog-footer";
+export {
+  MainLayout,
+  MainLayoutContainer,
+} from "./components/main-layout/main-layout";
+export { SidebarNav } from "./components/sidebar-nav/sidebar-nav";
+export { SidebarNavLogo } from "./components/sidebar-nav/sidebar-nav-logo";
+export { SidebarNavBtn } from "./components/sidebar-nav/sidebar-nav-btn";
+export type { onChangeEvent } from "./components/data-grid/data-grid.types";
+export type { DropdownOption } from "./components/dropdown/dropdown.types";
+export { Dropdown } from "./components/dropdown/dropdown";
+export { MonthsSwitch } from "./components/months-switch/months-switch";
+export { Scale } from "./components/scale/scale";
+export { Loader } from "./components/loader/loader";
